d0e62c3820360565976b038a11f6057cc6026809
tarnishablec <tarnishablec@outlook.com>
2019-07-24 17:41:39 +0800

    rise fix

--- src/components/background/rise.vue
+++ src/components/background/rise.vue
@@ -17,14 +17,14 @@
 		},
 		computed: {
 			style() {
-				return () => {
-					let len = `${_.random(40, 110)}px`;
+				return i => {
+					let len = `${_.random(10, 50)}px`;
 					return {
-						left: `${_.random(10, 90)}%`,
+						left: `${_.random(10 / i, 90 / i) * i}%`,
 						width: len,
 						height: len,
 						'animation-delay': `${_.random(1, 15, true)}s`,
-						'animation-duration': `${_.random(7, 23, true)}s`,
+						'animation-duration': `${_.random(10, 30, true)}s`,
 					}
 				}
 			}
@@ -33,7 +33,7 @@
 </script>
 
 <style lang="scss" scoped>
-	@keyframes rotate {
+	@keyframes rotate-rise {
 		0% {
 			transform: translateY(0) rotate(0deg);
 			opacity: 1;
@@ -55,11 +55,9 @@
 			position: absolute;
 			display: block;
 			list-style: none;
-			width: 2rem;
-			height: 2rem;
-			bottom: -130px;
+			bottom: -50px;
 			background: rgba(255, 255, 255, 0.2);
-			animation: rotate 20s linear infinite;
+			animation: rotate-rise 20s linear infinite;
 		}
 	}
 </style>

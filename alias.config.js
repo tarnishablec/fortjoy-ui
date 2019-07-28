e70c059c8b0a3312d9e1dd7c4f85be8b5cbdd7ed
tarnishablec <tarnishablec@outlook.com>
2019-07-29 01:42:26 +0800

    clean code

--- alias.config.js
+++ alias.config.js
@@ -1,3 +1,7 @@
+function resolve(dir) {
+	return path.join(__dirname, dir)
+}
+
 module.exports = {
 	resolve: {
 		alias: {
--- src/components/background/riseBackground.vue
+++ src/components/background/riseBackground.vue
@@ -45,7 +45,8 @@
 	}
 
 	.box-area {
-		background: linear-gradient(to right, #3a6186, #89253e);
+		/*background: linear-gradient(to right, #3a6186, #89253e);*/
+		background: #1c1c1c;
 		position: absolute;
 		width: 100%;
 		height: 100%;

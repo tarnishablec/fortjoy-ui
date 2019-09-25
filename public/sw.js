271d86dcc1076092890af6cc217f20de9e1efa37
tarnishablec <tarnishablec@outlook.com>
2019-09-25 18:21:05 +0800

    pwa support update

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,24 @@
+const staticCacheName = 'site-static';
+const assets = [
+  "/",
+];
+
 self.addEventListener("install", evt => {
-  console.log(`service worker has been installed`);
-  console.log(evt);
+  // console.log(`service worker has been installed`);
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  )
 });
 
-self.addEventListener("activate", evt => {
-  console.log(`service worker has been activated!`)
-});
+// self.addEventListener("activate", evt => {
+//   console.log(`service worker has been activated!`)
+// });
+
+self.addEventListener("fetch", evt => {
+  // console.log(evt)
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => cacheRes || fetch(evt.request))
+  )
+})

795bceca6f10dc439aab9ae6b5063d1138ff1c63
tarnishablec <tarnishablec@outlook.com>
2019-09-26 14:02:57 +0800

    fix hamberger

--- src/main.js
+++ src/main.js
@@ -4,13 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import _ from 'lodash'
-
 import '@/style/index.scss'
 
 Vue.config.productionTip = false;
-
-Vue.prototype.$loda = _;
 
 new Vue({
 	router,
